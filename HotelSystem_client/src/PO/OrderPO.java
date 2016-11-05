9c42ca9fc1297bda5c4e324c64e152d52ca01219
Loickandlorenzo <1220864925@qq.com>
2016-11-05 20:32:18 +0800

    合并到master

--- HotelSystem_client/src/PO/OrderPO.java
+++ HotelSystem_client/src/PO/OrderPO.java
@@ -2,6 +2,7 @@ package PO;
 
 public class OrderPO {
 	
+	
 	private int id;
 	
 	private int hotelId;
@@ -14,5 +15,24 @@ public class OrderPO {
 	
 	private String lastTime;
 	
-//	private String orderInfo;
+	private String orderInfo;
+	
+	private int price;
+	
+//	public OrderPo(){
+//		super();
+//	}
+//
+//	public OrderPo(int id, int hotelId, int userId, int status,
+//			String entryTime, String lastTime,String orderInfo,int price) {
+//		super();
+//		this.id = id;
+//		this.hotelId = hotelId;
+//		this.userId = userId;
+//		this.status = status;
+//		this.entryTime = entryTime;
+//		this.lastTime = lastTime;
+//		this.orderInfo = orderInfo;
+//		this.price = price;
+//	}
 }
