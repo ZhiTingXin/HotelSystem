ce52fa1409b93fa2dcecc73c3704a1bfb41c2226
ZhiTingXin <151250170@smail.nju.edu.cn>
2016-11-05 20:19:06 +0800

    辛志庭1.0

--- HotelSystem_client/src/presentation/controller/systemstrategyController/SystemStrategyController.java
+++ HotelSystem_client/src/presentation/controller/systemstrategyController/SystemStrategyController.java
@@ -1,5 +1,44 @@
 package presentation.controller.systemstrategyController;
 
-public class SystemStrategyController {
+import java.util.ArrayList;
+
+import VO.SystemStrategyVO;
+import blservice.SystemStrategy_blservice;
+import blservice.impl.SystemStrategy_bl;
+import other.ResultMessage;
+import presentation.view.systemStrategy_ui.SystemStrategy;
+
+public class SystemStrategyController implements  SystemStrategy{
+	
+	private SystemStrategy_blservice systemStrategy_blservice;
+	
+	public SystemStrategyController(){
+		systemStrategy_blservice  = new SystemStrategy_bl();
+	}
+
+	public ArrayList<SystemStrategyVO> getAllSystemStrategys(String systemStaffID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public SystemStrategyVO getSystemStrategy(String systemStrategy_Name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultMessage deleteSystemStrategy(String systemStrategy_Name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultMessage modifySystemStrategy(SystemStrategyVO systemStrategy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public SystemStrategyVO makeSystemStrategy(SystemStrategyVO systemStrategy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
