3858fdb8c2ebf48f99b1d4f1cd6785bd17f6675a
ZhiTingXin <151250170@smail.nju.edu.cn>
2016-11-08 16:40:32 +0800

    添加了对于SystemManagerInfo

--- HotelSystem_client/src/blservice/UserInfo_blservice.java
+++ HotelSystem_client/src/blservice/UserInfo_blservice.java
@@ -1,5 +1,11 @@
 package blservice;
 
+import VO.SystemManagerVO;
+import other.ResultMessage;
+
 public interface UserInfo_blservice {
 
+	public SystemManagerVO getSystemManagerInfo(String SystemManager_name);
+	
+	public ResultMessage modifySystemManagerInfo(SystemManagerVO systemManager);
 }
---
+++ HotelSystem_client/src/presentation/view/userInfo_ui/SystemManagerInfoControllerService.java
@@ -0,0 +1,12 @@
+package presentation.view.userInfo_ui;
+
+import VO.SystemManagerVO;
+import other.ResultMessage;
+
+public interface SystemManagerInfoControllerService {
+
+	public ResultMessage modifySystemManagerInfo(SystemManagerVO systemManager);
+	
+	public SystemManagerVO getSystemManagerInfo(String systemManager_Name);
+	
+}
