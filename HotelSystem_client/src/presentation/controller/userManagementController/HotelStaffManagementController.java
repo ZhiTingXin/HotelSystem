328d2de1a696cc54c024bb84e7ed842bf0739a1e
ZhiTingXin <151250170@smail.nju.edu.cn>
2016-11-12 20:05:28 +0800

    2016/11/12

--- HotelSystem_client/src/presentation/controller/userManagementController/HotelStaffManagementController.java
+++ HotelSystem_client/src/presentation/controller/userManagementController/HotelStaffManagementController.java
@@ -3,6 +3,7 @@ package presentation.controller.userManagementController;
 import java.util.ArrayList;
 
 import VO.HotelStaffVO;
+import other.ResultMessage;
 import presentation.view.userManagement_ui.HotelStaffManagementControllerService;
 
 public class HotelStaffManagementController implements HotelStaffManagementControllerService{
@@ -14,7 +15,7 @@ public class HotelStaffManagementController implements HotelStaffManagementContr
 		return list;
 	}
 
-	public boolean modifyHotelStaffManagement(HotelStaffVO hotelstaff, HotelStaffVO hotelstaffChange) {
+	public ResultMessage modifyHotelStaffManagement(HotelStaffVO hotelstaff) {
 		return false;
 	}
 
