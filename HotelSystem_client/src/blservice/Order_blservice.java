d489ac8855b04c2d57784fed57584df2fbf7792b
ZhiTingXin <151250170@smail.nju.edu.cn>
2016-11-21 14:52:35 +0800

    database1

--- HotelSystem_client/src/blservice/Order_blservice.java
+++ HotelSystem_client/src/blservice/Order_blservice.java
@@ -17,11 +17,9 @@ public interface Order_blservice {
 	public ArrayList<OrderPO> getUnfinishedOrders(int userID);
 	public ArrayList<OrderPO> getAbnomalOrders(int userID);
 	public ArrayList<OrderPO> getHotelOrders(int userID,int hotelID);
-
 	public boolean checkRoomType(int hotelID, RoomType type);
 	public ArrayList<PrivilegePO> getRecommendations(int userID, int hotelID);
 	public boolean generateOrder(OrderPO aOrder);
-	
 	public  boolean changeState(String orderID);
 	public boolean changeCredit(String userID,String orderID);
 }
